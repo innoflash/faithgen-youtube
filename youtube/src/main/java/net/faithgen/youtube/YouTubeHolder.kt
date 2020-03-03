1215b4e1ef799f479323538df35a83a8fe3cac6f
innoflash <innocentmazando@gmail.com>
2020-03-03 14:30:39 +0200

    converted youtube holder

---
+++ youtube/src/main/java/net/faithgen/youtube/YouTubeHolder.kt
@@ -0,0 +1,6 @@
+package net.faithgen.youtube
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+final class YouTubeHolder(val youTubeView: YouTubeView) : RecyclerView.ViewHolder(youTubeView) {}
